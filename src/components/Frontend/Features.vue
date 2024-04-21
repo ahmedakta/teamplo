3940b5cec42124bff2d52c153a19b591165b1634
ahmdpt101 <ahmdpt101@gmail.com>
2024-04-21 20:54:59 +0300

    added features section to home page

---
+++ src/components/Frontend/Features.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- Features Section -->
+  <section class="bg-white dark:bg-gray-900">
+    <div
+      class="max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28"
+    >
+      <h1 class="text-center text-[2rem]">Featrues</h1>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
--- src/views/Frontend/Index.vue
+++ src/views/Frontend/Index.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class=""></div>
   <Hero />
+  <Features />
 </template>
 
 <script setup>
+import Features from '@/components/Frontend/Features.vue'
 import Hero from '@/components/Frontend/Hero.vue'
 </script>

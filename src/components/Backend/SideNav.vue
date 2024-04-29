09e4fa6f7017f72f711815e9bf92b79bb90c6f8d
ahmdpt101 <ahmdpt101@gmail.com>
2024-04-30 01:57:39 +0300

    Side Nav frontend

--- src/components/Backend/SideNav.vue
+++ src/components/Backend/SideNav.vue
@@ -3,38 +3,51 @@
     <!-- Logo -->
     <div class="text-2xl text-black font-bold">
       <img alt="Vue logo" class="h-[4rem] logo px-10 py-2" src="@/assets/teamplo-logo-v1.png" />
+      <!-- General -->
+      <p class="text-gray-500 text-sm py-3">General <hr/></p>
+      <!-- Navigation Links -->
+      <nav class="mt-6 text-[1.1rem] text-gray-500">
+        <RouterLink to="/dashboard"           
+                    :class="{ active: $route.path === '/dashboard' }"
+                     class="block py-2 px-4 hover:bg-[#D6E6FD] hover:text-white">
+          <font-awesome-icon :icon="['fas', 'circle-notch']" />
+          Dashboard
+        </RouterLink>
+        <RouterLink to="/projects" 
+        :class="{ active: $route.path === '/projects' }"
+        class="block py-2 px-4 hover:bg-[#D6E6FD] hover:text-white">
+          <font-awesome-icon :icon="['fa', 'diagram-project']" />
+          Projects
+        </RouterLink>
+        <RouterLink to="/tasks" 
+            :class="{ active: $route.path === '/tasks' }"
+            class="block py-2 px-4 hover:bg-[#D6E6FD] hover:text-white">
+          <font-awesome-icon :icon="['fa', 'bars-progress']" />
+          Tasks
+        </RouterLink>
+        <RouterLink to="/events" 
+        :class="{ active: $route.path === '/events' }"
+        class="block py-2 px-4 hover:bg-[#D6E6FD] hover:text-white">
+          <font-awesome-icon :icon="['fa', 'calendar']" />
+          Events
+        </RouterLink>
+        <RouterLink to="/chats"
+        :class="{ active: $route.path === '/chats' }"
+        class="block py-2 px-4 hover:bg-[#D6E6FD] hover:text-white">
+          <font-awesome-icon :icon="['fa', 'comment']" />
+          Chats
+        </RouterLink>
+        <a
+          href="#"
+          class="block py-2 px-4 text-red-500 hover:bg-[#D6E6FD]"
+          @click="logout()"
+          v-if="userStore.id">
+          <font-awesome-icon :icon="['fa', 'right-from-bracket']" />
+          Logout
+        </a>
+        <!-- Add more links as needed -->
+      </nav>
     </div>
-    <!-- Navigation Links -->
-    <nav class="mt-6 text-gray-500">
-      <RouterLink to="/dashboard" class="block py-2 px-4 hover:bg-gray-700 hover:text-white">
-        <font-awesome-icon :icon="['fas', 'circle-notch']" />
-        Dashboard
-      </RouterLink>
-      <RouterLink to="/projects" class="block py-2 px-4 hover:bg-gray-700 hover:text-white">
-        <font-awesome-icon :icon="['fa', 'diagram-project']" />
-        Projects
-      </RouterLink>
-      <RouterLink to="/tasks" class="block py-2 px-4 hover:bg-gray-700 hover:text-white">
-        <font-awesome-icon :icon="['fa', 'bars-progress']" />
-        Tasks
-      </RouterLink>
-      <RouterLink to="/events" class="block py-2 px-4 hover:bg-gray-700 hover:text-white">
-        <font-awesome-icon :icon="['fa', 'calendar']" />
-        Events
-      </RouterLink>
-      <RouterLink to="/chats" class="block py-2 px-4 hover:bg-gray-700 hover:text-white">
-        <font-awesome-icon :icon="['fa', 'comment']" />
-        Chats
-      </RouterLink>
-      <a
-        href="#"
-        class="block py-2 px-4 text-red-500 hover:bg-gray-700"
-        @click="logout()"
-        v-if="userStore.id"
-        >Logout</a
-      >
-      <!-- Add more links as needed -->
-    </nav>
     <!-- Footer (Optional) -->
     <div class="mt-6 text-gray-500"><p>Made By Teamplo</p></div>
   </div>
@@ -60,3 +73,10 @@ const logout = () => {
   }
 }
 </script>
+<style scoped>
+.active{
+  background-color: #D6E6FD;
+ color: #1038FA;
+ border-radius: 10px;
+ font:bolder;
+}</style>

a11b4c63c30e434c85c1bec9f09286621e1af923
ahmedakta <ahmdekta@gmail.com>
2024-06-15 22:48:55 +0300

    changes

--- src/views/Backend/Forms/ProjectForm.vue
+++ src/views/Backend/Forms/ProjectForm.vue
@@ -1,7 +1,6 @@
 <template>
   <MainLayout>
     <div class="mx-auto p-4">
-
       <div
         :class="{ 'skeleton-loader bg-gray-200 animate-pulse': generalStore.isLoading }"
         class="bg-white shadow-md rounded-lg p-6"
@@ -10,19 +9,35 @@
         <div class="grid grid-cols-2 gap-4">
           <div>
             <h3 class="text-l font-semibold">Project Name</h3>
-            <input v-model="data.value.project_name" class="border w-1/2 border-gray-300 rounded-md py-2 pl-3 pr-4 focus:outline-none focus:border-blue-500">
+            <input
+              v-model="data.value.project_name"
+              class="border w-1/2 border-gray-300 rounded-md py-2 pl-3 pr-4 focus:outline-none focus:border-blue-500"
+            />
           </div>
           <div>
             <h3 class="text-l font-semibold">Project Details</h3>
             <textarea
-            v-model="data.value.project_description"
-            class="border w-1/2 border-gray-300 rounded-md  focus:outline-none focus:border-blue-500"
-            rows="2"
-          ></textarea>
+              v-model="data.value.project_description"
+              class="border w-1/2 border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+              rows="2"
+            ></textarea>
           </div>
           <div>
             <h3 class="text-l font-semibold">Department ID</h3>
-            <p class="text-gray-600">1</p>
+            <select
+              v-model="data.value.department_id"
+              name=""
+              id=""
+              class="border w-1/2 border-gray-300 rounded-md py-2 pl-3 pr-4 focus:outline-none focus:border-blue-500"
+            >
+              <option
+                v-for="(department, key) in generalStore.data.departments"
+                :key="key"
+                :value="department.id"
+              >
+                {{ department.department_name }}
+              </option>
+            </select>
           </div>
           <div>
             <h3 class="text-l font-semibold">Budget</h3>
@@ -42,7 +57,7 @@
           <div>
             <h3 class="text-l font-semibold">Start Date</h3>
             <input
-            v-model="data.value.project_start_at"
+              v-model="data.value.project_start_at"
               class="border-b w-60 border-gray-300 rounded-md py-2 pl-3 pr-4 focus:outline-none focus:border-blue-500"
               type="date"
             />
@@ -50,7 +65,7 @@
           <div>
             <h3 class="text-l font-semibold">End Date</h3>
             <input
-            v-model="data.value.project_end_at"
+              v-model="data.value.project_end_at"
               class="border-b w-60 border-gray-300 rounded-md py-2 pl-3 pr-4 focus:outline-none focus:border-blue-500"
               type="date"
             />
@@ -59,7 +74,7 @@
           <div>
             <h3 class="text-l font-semibold">Priority</h3>
             <select
-            v-model="data.value.project_priority"
+              v-model="data.value.project_priority"
               class="border-b w-60 border-gray-300 rounded-md py-2 pl-3 pr-4 focus:outline-none focus:border-blue-500"
               name="status"
             >
@@ -146,15 +161,24 @@
                 </div>
               </div>
             </div>
+          </div>
         </div>
-    </div>
         <!-- Actions Section -->
-        <div class="float-right ">
-            <button class="text-white  bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-10 py-3 mr-2  dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Cancel</button>
-            <button class="text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-3  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" @click="generalStore.makeRequest('api/project/save', data, 'POST')">Save</button>
+        <div class="float-right">
+          <button
+            class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-10 py-3 mr-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+          >
+            Cancel
+          </button>
+          <button
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            @click="generalStore.makeRequest('api/project/save', data, 'POST')"
+          >
+            Save
+          </button>
         </div>
-</div>
-</div>
+      </div>
+    </div>
   </MainLayout>
 </template>
 <script setup>
@@ -164,18 +188,26 @@ import { ref, onMounted } from 'vue'
 
 const generalStore = useGeneralStore()
 const data = ref({
-    value:{
-        project_name: '',
-        project_description: '',
-        stage: '',
-        users: '',
-        department: '',
-        project_start_at: '',
-        project_end_at: '',
-        status: '',
-    }
+  value: {
+    project_name: '',
+    department_id: '',
+    project_description: '',
+    stage: '',
+    users: '',
+    project_start_at: '',
+    project_end_at: '',
+    status: ''
+  }
 })
-onMounted(() => {
-
+onMounted(async () => {
+  try {
+    generalStore.getData('api/projects/create')
+    const response = await axios.get('url', {
+      withCredentials: true,
+      params
+    })
+  } catch (error) {
+    console.log(error)
+  }
 })
 </script>

9438ce2c10e66844da85af744c173153a36579fe
ahmedakta <ahmdekta@gmail.com>
2024-07-12 01:13:19 +0300

    changes

--- src/views/Backend/DepartmentView.vue
+++ src/views/Backend/DepartmentView.vue
@@ -1,69 +1,32 @@
 <template>
   <MainLayout>
-    <div class="p-4 md:p-8">
-      <h1 class="text-3xl font-bold mb-4">{{ department.department_name }}</h1>
-      <p class="text-gray-600 mb-8">{{ department.department_desc }}</p>
-
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <!-- Todo Column -->
-        <div class="bg-white p-4 rounded-lg shadow">
-          <h2 class="text-xl font-semibold mb-2">Todo</h2>
-          <ul>
-            <li v-for="task in tasks" :key="task.id" class="mb-2 p-2 bg-gray-100 rounded-lg">
-              <h3 class="text-lg font-semibold mb-1">{{ task.name }}</h3>
-              <div class="text-sm text-gray-700 mb-2">{{ task.description }}</div>
-              <div class="flex space-x-4 mb-2">
-                <button @click="togglePriority" class="flex items-center">
-                  <i :class="priorityIcon" class="fas fa-lg"></i>
-                  <font-awesome-icon :icon="['fas', 'flag']" />
-                </button>
-
-                <button @click="assignUser" class="flex items-center">
-                  <i class="fas fa-user fa-lg"></i>
-                  <font-awesome-icon :icon="['fas', 'user']" />
-                </button>
-
-                <button @click="startTimer" class="flex items-center">
-                  <font-awesome-icon :icon="['fas', 'stopwatch']" />
-                </button>
-
-                <button @click="toggleStatus" class="flex items-center">
-                  <i :class="statusIcon" class="fas fa-lg"></i>
-                  <font-awesome-icon :icon="['fas', 'check']" />
-                </button>
-              </div>
-            </li>
-          </ul>
-        </div>
-
-        <!-- In Progress Column -->
-        <div class="bg-white p-4 rounded-lg shadow">
-          <h2 class="text-xl font-semibold mb-2">In Progress</h2>
-          <ul>
-            <li
-              v-for="task in inProgressTasks"
-              :key="task.id"
-              class="mb-2 p-2 bg-yellow-100 rounded-lg"
-            >
-              {{ task.name }}
-            </li>
-          </ul>
-        </div>
-
-        <!-- Cancelled Column -->
-        <div class="bg-white p-4 rounded-lg shadow">
-          <h2 class="text-xl font-semibold mb-2">Cancelled</h2>
-          <ul>
-            <li
-              v-for="task in cancelledTasks"
-              :key="task.id"
-              class="mb-2 p-2 bg-red-100 rounded-lg"
-            >
-              {{ task.name }}
-            </li>
-          </ul>
-        </div>
+    <div>
+      <div class="flex justify-center space-x-2 mb-8">
+        <button class="bg-white px-10 py-2 border-black rounded-lg text-black">
+          <!-- :class="tabClass(tab)"
+          @click="selectedTab = tab" -->
+          Projects
+        </button>
+        <button class="border-[1px] border-gray-500 px-10 py-2 rounded-lg text-black">KPI</button>
+        <button class="border-[1px] border-gray-500 px-10 py-2 rounded-lg text-black">
+          Employee's
+        </button>
+        {{ activeTab }}
       </div>
+      <!-- <div v-if="selectedTab === 'KPI'">
+        <KpiSection />
+      </div>
+      <div v-else-if="selectedTab === 'TASKS'">
+        <TasksSection
+          :tasks="tasks"
+          :inProgressTasks="inProgressTasks"
+          :cancelledTasks="cancelledTasks"
+        />
+      </div>
+      <div v-else-if="selectedTab === 'USERS'">
+        <UsersSection :users="users" />
+      </div> -->
+      <Kpi :department="department" :tasks="tasks" />
     </div>
   </MainLayout>
 </template>
@@ -71,12 +34,13 @@
 import MainLayout from '@/layouts/Backend/MainLayout.vue'
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
-import axios from 'axios'
+import Kpi from '@/components/Backend/Departments/Kpi.vue'
 import { useGeneralStore } from '@/stores/general'
 const generalStore = useGeneralStore()
 const route = useRoute()
 const department = ref({})
 const tasks = ref({})
+let activeTab = ref('kpi')
 
 onMounted(() => {
   try {

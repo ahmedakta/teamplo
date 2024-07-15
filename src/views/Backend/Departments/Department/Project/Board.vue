926aad505f16812f7f5a9fba8624887684c80ed5
ahmedakta <ahmdekta@gmail.com>
2024-07-16 02:45:28 +0300

    changes on frontend

--- src/views/Backend/Departments/Department/Project/Board.vue
+++ src/views/Backend/Departments/Department/Project/Board.vue
@@ -1,17 +1,108 @@
 <template>
   <MainLayout>
     <div class="p-4 md:p-8">
-      <!-- <h1 class="text-3xl font-bold mb-4">{{ department.department_name }}</h1>
-      <p class="text-gray-600 mb-8">{{ department.department_desc }}</p> -->
+      <h1 class="text-2xl font-bold mb-4" v-if="data.department">
+        {{ data.department.department_name }} - {{ data.project.project_name }} project
+      </h1>
+      <!-- Filter Section -->
+      <button
+        class="text-blue-700 border-blue-700 border-[1px] hover:bg-blue-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-5 focus:outline-none w-full md:w-auto"
+        type="button"
+        @click="isOpen = !isOpen"
+      >
+        <font-awesome-icon :icon="['fa', 'bars']" />
+      </button>
+      <!-- End Of  Filter Section -->
       <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
-        <!-- Todo Column -->
         <div class="bg-gray-200 p-4 rounded-lg shadow">
-          <h2 class="text-xl font-semibold mb-2 rounded-lg p-2 bg-orange-100">Todo</h2>
+          <div class="flex items-center justify-between mb-2 p-2 bg-orange-100 rounded-lg">
+            <h2 class="text-xl font-semibold">Todo</h2>
+            <!-- buttons section -->
+            <div class="text-gray-600 space-x-2">
+              <button class="bg-white py-1 px-2 rounded-lg">
+                <font-awesome-icon :icon="['fas', 'bars']" />
+              </button>
+              <button class="bg-white py-1 px-2 rounded-lg">
+                <font-awesome-icon :icon="['fas', 'plus']" />
+              </button>
+            </div>
+          </div>
           <ul>
-            <li v-for="task in data" :key="task.id" class="mb-2 p-2 bg-white rounded-lg">
-              <h3 class="text-lg font-semibold mb-1">{{ task.name }}</h3>
+            <li v-for="task in data.tasks" :key="task.id" class="mb-2 p-2 bg-white rounded-lg">
+              <h3 class="text-lg font-semibold mb-1">
+                <font-awesome-icon class="cursor-grab" :icon="['fas', 'grip']" /> {{ task.name }}
+              </h3>
               <div class="text-sm text-gray-700 mb-2">{{ task.description }}</div>
               <div class="flex space-x-4 mb-2">
+                <div class="flex items-center">
+                  <!-- Assigned Users Section -->
+                  <div class="w-40">
+                    <!-- Display assigned users -->
+                    <div class="flex items-center justify-between border-b">
+                      <div class="flex items-center">
+                        <div class="flex -space-x-2">
+                          <div class="relative group" data-username="User 1">
+                            <img
+                              class="w-10 h-10 rounded-full border-2 border-white"
+                              src="@/assets/default_profile_image.png"
+                              alt="User 1"
+                            />
+                            <div
+                              class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 text-white text-xl rounded-full opacity-0 group-hover:opacity-100"
+                            >
+                              ×
+                            </div>
+                            <div
+                              class="absolute -top-6 left-1/2 transform -translate-x-1/2 text-xs text-white bg-gray-800 px-4 py-1 rounded opacity-0 group-hover:opacity-100"
+                            >
+                              User 1
+                            </div>
+                          </div>
+                          <div class="relative group">
+                            <img
+                              class="w-10 h-10 rounded-full border-2 border-white"
+                              src="@/assets/default_profile_image.png"
+                              alt="User 1"
+                            />
+                            <div
+                              class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 text-white text-xl rounded-full opacity-0 group-hover:opacity-100"
+                            >
+                              ×
+                            </div>
+                            <div
+                              class="absolute mb-100 inset-0 flex items-center justify-center bg-black bg-opacity-60 text-black text-xl rounded-full opacity-0 group-hover:opacity-100"
+                            >
+                              a
+                            </div>
+                          </div>
+                          <div class="relative group">
+                            <img
+                              class="w-10 h-10 rounded-full border-2 border-white"
+                              src="@/assets/default_profile_image.png"
+                              alt="User 1"
+                            />
+                            <div
+                              class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60 text-white text-xl rounded-full opacity-0 group-hover:opacity-100"
+                            >
+                              ×
+                            </div>
+                          </div>
+                        </div>
+                        <div
+                          class="flex items-center justify-center w-10 h-10 text-green-800 bg-green-100 rounded-full"
+                        >
+                          +
+                        </div>
+                      </div>
+                      <!-- <button
+                    @click="removeUser(index)"
+                    class="text-red-500 hover:text-red-700 focus:outline-none"
+                  >
+                    Remove
+                  </button> -->
+                    </div>
+                  </div>
+                </div>
                 <button @click="togglePriority" class="flex items-center">
                   <i :class="priorityIcon" class="fas fa-lg"></i>
                   <font-awesome-icon :icon="['fas', 'flag']" />

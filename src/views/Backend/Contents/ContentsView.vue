8a58e6aedb00f62b87601e2d791f3710b03d18f0
ahmedakta <ahmdekta@gmail.com>
2024-07-20 01:01:56 +0300

    imported datatable to contents

--- src/views/Backend/Contents/ContentsView.vue
+++ src/views/Backend/Contents/ContentsView.vue
@@ -1,31 +1,81 @@
 <template>
     <MainLayout>
       <div class="container max-w-[7xl] mx-auto flex flex-wrap justify-between">
-        <!-- KPI cards -->
-        <div class="flex flex-wrap justify-between mb-5">
-          <!-- KPI Card 1 -->
-          <div
-            class="bg-white rounded-lg shadow-md p-6 w-full sm:w-[calc(50%-2rem)] md:w-[23.5rem] m-1"
-          >
-            <div class="flex items-center justify-between">
-              <span class="text-gray-700 text-sm">Total Pages:</span>
-              <span class="text-gray-700 text-sm">0</span>
-            </div>
-          </div>
-          <div
-            class="bg-white rounded-lg shadow-md p-6 w-full sm:w-[calc(50%-2rem)] md:w-[23.5rem] m-1"
-          >
-            <div class="flex items-center justify-between">
-              <span class="text-gray-700 text-sm">Total Categories:</span>
-              <span class="text-gray-700 text-sm">0</span>
-            </div>
-          </div>
-        </div>
+        <vue3-datatable :rows="rows" :columns="cols">
+        </vue3-datatable>
       </div>
     </MainLayout>
   </template>
   
-  <script setup>
+  <script setup lang="ts">
   import MainLayout from '@/layouts/Backend/MainLayout.vue'
+  import { ref } from "vue";
+  import Vue3Datatable from "@bhplugin/vue3-datatable";
+  import "@bhplugin/vue3-datatable/dist/style.css";
+
+  const cols =
+    ref([
+      { field: "id", title: "ID", width: "90px", filter: false },
+      { field: "name", title: "Name" },
+      { field: "username", title: "Username" },
+      { field: "email", title: "Email" },
+      { field: "phone", title: "Phone" },
+      { field: "date", title: "Date", type: "date" },
+      { field: "active", title: "Active", type: "bool" },
+      { field: "age", title: "Age", type: "number" },
+      {
+        field: "address.city",
+        title: "Address",
+        search: true,
+        cellRenderer: (item: any) => {
+          return (
+            item &&
+            item.address?.street +
+              ", " +
+              item.address?.suite +
+              "<br/><strong>" +
+              item.address?.city +
+              "</strong>" +
+              "<br/><strong>" +
+              "lat: " +
+              item.address?.geo?.lat +
+              "&nbsp;&nbsp; lng: " +
+              item.address?.geo?.lng +
+              "</strong>"
+          );
+        },
+      },
+      { field: "company.name", title: "Company" },
+    ]) || [];
+
+  const rows = ref([
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz",
+      address: {
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+        geo: {
+          lat: "-37.3159",
+          lng: "81.1496",
+        },
+      },
+      phone: "1-770-736-8031 x56442",
+      website: "hildegard.org",
+      company: {
+        name: "Romaguera-Crona",
+        catchPhrase: "Multi-layered client-server neural-net",
+        bs: "harness real-time e-markets",
+      },
+      date: "Tue Sep 27 2022 22:19:57",
+      active: true,
+      age: 10,
+    },
+
+  ]);
   </script>
   

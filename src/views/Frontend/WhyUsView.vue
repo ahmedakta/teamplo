f6e3737d4bd96096ab52f3ee8bb3b9ca8ea29000
ahmedakta <ahmdekta@gmail.com>
2024-07-25 01:47:21 +0300

    changes

--- src/views/Frontend/WhyUsView.vue
+++ src/views/Frontend/WhyUsView.vue
@@ -1,123 +1,131 @@
 <template>
-    <MainLayout>
-        <header class="mx-auto text-white relative">
-            <img src="../../assets/blogs/header.png" alt="Image 1" class="w-full rounded-lg shadow-lg" />
-            <h3 class=" text-black absolute bg-blue inset-0 flex items-center justify-center font-semibold text-4xl">
-                Why Us
-            </h3>
-        </header>
+  <MainLayout>
+    <header class="mx-auto text-white relative">
+      <img src="../../assets/blogs/header.png" alt="Image 1" class="w-full rounded-lg shadow-lg" />
+      <h3
+        class="text-black absolute bg-blue inset-0 flex items-center justify-center font-semibold text-4xl"
+      >
+        Why Us
+      </h3>
+    </header>
 
-        <section class="py-32 h-full">
-            <div class="container mx-auto px-4 text-black ">
-                <div class="grid md:grid-cols-2 gap-8 h-24 mb-10">
-                    <div>
-                        <h3 class="text-4xl font-bold">Features</h3>
-                    </div>
-                    <div>
-                        <h3 class=" mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facilis
-                            placeat praesentium eius commodi alias harum molestiae exercitationem similique magni?</h3>
-
-                    </div>
-                </div>
-                <div class="grid md:grid-cols-2 gap-8 h-auto mb-10">
-                    <!-- Feature Image -->
-                    <div class="w-full">
-                        <img src="../../assets/features-1.png" width="100%" alt="">
-                    </div>
-                    <!-- Feature List -->
-                    <div class="mt-auto mb-auto">
-                        <h3 class="text-3xl text-left font-bold mb-4">Departments and projects managment</h3>
-                        <ul>
-                            <li>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo saepe
-                                a doloribus
-                                unde
-                                pariatur quidem praesentium vitae. Obcaecati.</li>
-                            <li>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo saepe
-                                a doloribus
-                                unde
-                                pariatur quidem praesentium vitae. Obcaecati.</li>
-                        </ul>
-                        <button class="text-white ml-1 py-2 px-12 m-4 rounded bg-[#3AA9D0]">
-                            Contact Us
-                            <font-awesome-icon :icon="['fas', 'arrow-right']" />
-                        </button>
-                    </div>
-                </div>
-                <div class="grid md:grid-cols-2 gap-8 h-auto mb-10">
-                    <!-- Feature List -->
-                    <div class="mt-auto mb-auto">
-                        <h3 class="text-3xl text-left font-bold mb-4">Departments and projects managment</h3>
-                        <ul>
-                            <li>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo saepe
-                                a doloribus
-                                unde
-                                pariatur quidem praesentium vitae. Obcaecati.</li>
-                            <li>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo saepe
-                                a doloribus
-                                unde
-                                pariatur quidem praesentium vitae. Obcaecati.</li>
-                        </ul>
-                        <button class="text-white ml-1 py-2 px-12 m-4 rounded bg-[#3AA9D0]">
-                            Contact Us
-                            <font-awesome-icon :icon="['fas', 'arrow-right']" />
-                        </button>
-                    </div>
-                    <!-- Feature Image -->
-                    <div class="w-full">
-                        <img src="../../assets/feature-2.png" width="100%" alt="">
-                    </div>
-                </div>
-                <div class="grid md:grid-cols-2 gap-8 h-auto mb-10">
-                    <!-- Feature Image -->
-                    <div class="w-full">
-                        <img src="../../assets/features-3.png" width="100%" alt="">
-                    </div>
-                    <!-- Feature List -->
-                    <div class="mt-auto mb-auto">
-                        <h3 class="text-3xl text-left font-bold mb-4">Departments and projects managment</h3>
-                        <ul>
-                            <li>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo saepe
-                                a doloribus
-                                unde
-                                pariatur quidem praesentium vitae. Obcaecati.</li>
-                            <li>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo saepe
-                                a doloribus
-                                unde
-                                pariatur quidem praesentium vitae. Obcaecati.</li>
-                        </ul>
-                        <button class="text-white ml-1 py-2 px-12 m-4 rounded bg-[#3AA9D0]">
-                            Contact Us
-                            <font-awesome-icon :icon="['fas', 'arrow-right']" />
-                        </button>
-                    </div>
-                </div>
-                <div class="grid md:grid-cols-2 gap-8 h-auto mb-10">
-                    <!-- Feature List -->
-                    <div class="mt-auto mb-auto">
-                        <h3 class="text-3xl text-left font-bold mb-4">Departments and projects managment</h3>
-                        <ul>
-                            <li>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo saepe
-                                a doloribus
-                                unde
-                                pariatur quidem praesentium vitae. Obcaecati.</li>
-                            <li>- Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo saepe
-                                a doloribus
-                                unde
-                                pariatur quidem praesentium vitae. Obcaecati.</li>
-                        </ul>
-                        <button class="text-white ml-1 py-2 px-12 m-4 rounded bg-[#3AA9D0]">
-                            Contact Us
-                            <font-awesome-icon :icon="['fas', 'arrow-right']" />
-                        </button>
-                    </div>
-                    <!-- Feature Image -->
-                    <div class="w-full">
-                        <img src="../../assets/features-4.png" width="100%" alt="">
-                    </div>
-                </div>
+    <section class="py-32 h-full">
+      <div class="container mx-auto px-4 text-black">
+        <div class="grid md:grid-cols-2 gap-12 h-[40rem]">
+          <!-- Feature List -->
+          <div class="mt-auto mb-auto">
+            <h3 class="text-3xl text-left font-bold mb-4">Departments and projects managment</h3>
+            <ul>
+              <li>
+                - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo
+                saepe a doloribus unde pariatur quidem praesentium vitae. Obcaecati.
+              </li>
+              <li>
+                - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo
+                saepe a doloribus unde pariatur quidem praesentium vitae. Obcaecati.
+              </li>
+            </ul>
+            <button class="text-white ml-1 py-2 px-12 m-4 rounded bg-[#3AA9D0]">
+              Contact Us
+              <font-awesome-icon :icon="['fas', 'arrow-right']" />
+            </button>
+          </div>
+          <!-- Feature Image -->
+          <div class="relative w-full">
+            <div class="relative flex mt-20 justify-center">
+              <img
+                src="../../assets/why-us-1.png"
+                alt="Image 1"
+                class="z-10 rounded-lg w-1/2 translate-y-40 -translate-x-60"
+              />
+              <img
+                src="../../assets/why-us-2.png"
+                alt="Image 2"
+                class="absolute inset-y-0 left-0 transform -translate-x-10 -translate-y-20 rounded-lg w-3/4"
+              />
+              <img
+                src="../../assets/why-us-3.png"
+                alt="Image 2"
+                class="absolute z-5 inset-y-0 right-0 transform -translate-x-120 translate-y-20 rounded-lg w-1/2"
+              />
             </div>
-        </section>
-    </MainLayout>
+          </div>
+        </div>
+        <div class="grid md:grid-cols-2 gap-12 h-auto mb-10">
+          <!-- Feature List -->
+          <div class="mt-auto mb-auto">
+            <h3 class="text-3xl text-left font-bold mb-4">Departments and projects managment</h3>
+            <ul>
+              <li>
+                - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo
+                saepe a doloribus unde pariatur quidem praesentium vitae. Obcaecati.
+              </li>
+              <li>
+                - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo
+                saepe a doloribus unde pariatur quidem praesentium vitae. Obcaecati.
+              </li>
+            </ul>
+            <button class="text-white ml-1 py-2 px-12 m-4 rounded bg-[#3AA9D0]">
+              Contact Us
+              <font-awesome-icon :icon="['fas', 'arrow-right']" />
+            </button>
+          </div>
+          <!-- Feature Image -->
+          <div class="w-full">
+            <img src="../../assets/feature-2.png" width="100%" alt="" />
+          </div>
+        </div>
+        <div class="grid md:grid-cols-2 gap-8 h-auto mb-10">
+          <!-- Feature Image -->
+          <div class="w-full">
+            <img src="../../assets/features-3.png" width="100%" alt="" />
+          </div>
+          <!-- Feature List -->
+          <div class="mt-auto mb-auto">
+            <h3 class="text-3xl text-left font-bold mb-4">Departments and projects managment</h3>
+            <ul>
+              <li>
+                - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo
+                saepe a doloribus unde pariatur quidem praesentium vitae. Obcaecati.
+              </li>
+              <li>
+                - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo
+                saepe a doloribus unde pariatur quidem praesentium vitae. Obcaecati.
+              </li>
+            </ul>
+            <button class="text-white ml-1 py-2 px-12 m-4 rounded bg-[#3AA9D0]">
+              Contact Us
+              <font-awesome-icon :icon="['fas', 'arrow-right']" />
+            </button>
+          </div>
+        </div>
+        <div class="grid md:grid-cols-2 gap-8 h-auto mb-10">
+          <!-- Feature List -->
+          <div class="mt-auto mb-auto">
+            <h3 class="text-3xl text-left font-bold mb-4">Departments and projects managment</h3>
+            <ul>
+              <li>
+                - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo
+                saepe a doloribus unde pariatur quidem praesentium vitae. Obcaecati.
+              </li>
+              <li>
+                - Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi beatae explicabo
+                saepe a doloribus unde pariatur quidem praesentium vitae. Obcaecati.
+              </li>
+            </ul>
+            <button class="text-white ml-1 py-2 px-12 m-4 rounded bg-[#3AA9D0]">
+              Contact Us
+              <font-awesome-icon :icon="['fas', 'arrow-right']" />
+            </button>
+          </div>
+          <!-- Feature Image -->
+          <div class="w-full">
+            <img src="../../assets/features-4.png" width="100%" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  </MainLayout>
 </template>
 <script setup>
 import MainLayout from '@/layouts/Frontend/MainLayout.vue'

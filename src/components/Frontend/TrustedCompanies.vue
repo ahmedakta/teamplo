da1c90a1aca6f4160ddedc907a4269eb7d2c13ed
ahmedakta <ahmdekta@gmail.com>
2024-07-29 21:12:46 +0300

    fixed trusted compnies frontend

--- src/components/Frontend/TrustedCompanies.vue
+++ src/components/Frontend/TrustedCompanies.vue
@@ -6,37 +6,37 @@
         <div class="w-full md:w-1/2 text-center md:text-left">
           <h2 class="text-xl font-semibold mb-4">Trusted by Global Companies</h2>
         </div>
-        <div class="md:w-1/2 flex text-center justify-between">
+        <div class="md:w-1/2 max-w-1/2 w-1/2 flex text-center justify-between">
           <!-- Company Icons -->
           <img
             src="../../assets/trusted_companies/company1.png"
             alt="Company 1"
-            class="md:h-10 md:w-full transition duration-300 transform hover:scale-110"
+            class="md:h-10 md:w-full w-16 p-1 transition duration-300 transform hover:scale-110"
           />
           <img
             src="../../assets/trusted_companies/company2.png"
             alt="Company 2"
-            class="md:h-10 md:w-full transition duration-300 transform hover:scale-110"
+            class="md:h-10 md:w-full w-16 p-1 transition duration-300 transform hover:scale-110"
           />
           <img
             src="../../assets/trusted_companies/company3.png"
             alt="Company 3"
-            class="md:h-10 md:w-full transition duration-300 transform hover:scale-110"
+            class="md:h-10 md:w-full w-16 p-1 transition duration-300 transform hover:scale-110"
           />
           <img
             src="../../assets/trusted_companies/company4.png"
             alt="Company 3"
-            class="md:h-10 md:w-full transition duration-300 transform hover:scale-110"
+            class="md:h-10 md:w-full w-16 p-1 transition duration-300 transform hover:scale-110"
           />
           <img
             src="../../assets/trusted_companies/company5.png"
             alt="Company 3"
-            class="md:h-10 md:w-full transition duration-300 transform hover:scale-110"
+            class="md:h-10 md:w-full w-16 p-1 transition duration-300 transform hover:scale-110"
           />
           <img
             src="../../assets/trusted_companies/company6.png"
             alt="Company 3"
-            class="md:h-10 md:w-full transition duration-300 transform hover:scale-110"
+            class="md:h-10 md:w-full w-16 p-1 transition duration-300 transform hover:scale-110"
           />
           <!-- Add more company icons as needed -->
         </div>
@@ -47,8 +47,8 @@
 
 <script setup></script>
 <style scoped>
-img {
+/* img {
   width: 4rem;
   padding: 0.3rem;
-}
+} */
 </style>
